fd9e0edfa39e97e81976ce1fc062905a36438a72
KevinLxj <13996393900@139.com>
2018-09-26 10:47:23 +0800

    commit

--- springboot-mybatis/src/main/java/com/example/springbootmybatis/SpringbootMybatisApplication.java
+++ springboot-mybatis/src/main/java/com/example/springbootmybatis/SpringbootMybatisApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //测试
 @SpringBootApplication
 public class SpringbootMybatisApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMybatisApplication.class, args);
 	}
